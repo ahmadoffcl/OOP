8bf9aceb09ffca88e34c45ad047649abe2ba51c6
Ahmed <ahmed148474@gmail.com>
2026-06-06 11:13:53 +0500

    Add utility implementation

---
+++ src/utils/Utils.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file    Utils.cpp
+ * @brief   Implementation of small helper functions
+ * @author  Ahmad Ali (25-CS-067), Umer Altaf (25-CS-057), Muhammed Ahmad (25-CS-252)
+ * @course  CS-104L: Object-Oriented Programming
+ * @inst    HITEC University Taxila
+ * @date    2026-06-05
+ *
+ * OOP Concepts: Utility Functions
+ */
+
+#include "Utils.h"
+#include <iostream>
+using namespace std;
+
+string Utils::getTodayDate() {
+    return "05-06-2026";
+}
+
+void Utils::printLine() {
+    cout << "------------------------------------------------------------" << endl;
+}
+
+void Utils::printSmallLine() {
+    cout << "------------------------------" << endl;
+}
+
+int Utils::checkPositiveInt(int value) {
+    if (value < 0) {
+        return 0;
+    }
+
+    return value;
+}
